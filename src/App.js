53af1fada6e60f5b6d20edb6afa514ccf487df71
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-08-12 18:58:47 -0400

    completed scorebord

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore * 0)}>Home Score Reset</button>
+           <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore * 0)}>Home Score Reset</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown"onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
